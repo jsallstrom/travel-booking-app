586b6f727be33f32381506c59a5730a1225e55b9
Johan Sällström <johansallstrom7@outlook.com>
2020-10-29 12:55:10 +0100

    Moved fetchData() to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import AppRouter from "./routers/AppRouter";
 
+import { DispatchContext } from "./context/StoreProvider";
+
+import axios from "axios";
+
 export default function App() {
+     const dispatch = useContext(DispatchContext);
+     const fetchData = async () => {
+          /** Cross-Origin begäran blockerad: Same-Origin policyn tillåter inte läsningar av 
+           * fjärresurs på https://webdev-exercise.netlify.app/data/products.json.
+           *  (Orsak: CORS header 'Access-Control-Allow-Origin' saknas).
+           * 
+           *  
+           const response = await axios.get(
+               "https://webdev-exercise.netlify.app/data/products.json",
+               
+          );
+           * 
+           */
+
+          const response = await axios.get("products.json");
+
+          const data = response.data;
+
+          dispatch({
+               type: "FETCH_DATA",
+               payload: data,
+          });
+
+          console.log(data);
+     };
+
+     useEffect(() => {
+          fetchData();
+     }, []);
      return <AppRouter></AppRouter>;
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,6 @@ import { DispatchContext, StateContext } from "../context/StoreProvider";
 
 import { Link } from "react-router-dom";
 
-import axios from "axios";
-
 // 1) make NavBar
 // 2) make simpel booking Page, able to add booking from there, use Formik?? Nah not needed
 // 3) Load in available bookings into Store from app
@@ -14,39 +12,6 @@ import axios from "axios";
 export default function HomePage() {
      const dispatch = useContext(DispatchContext);
      const state = useContext(StateContext);
-
-     const fetchData = async () => {
-          /** Cross-Origin begäran blockerad: Same-Origin policyn tillåter inte läsningar av 
-           * fjärresurs på https://webdev-exercise.netlify.app/data/products.json.
-           *  (Orsak: CORS header 'Access-Control-Allow-Origin' saknas).
-           * 
-           *  
-           const response = await axios.get(
-               "https://webdev-exercise.netlify.app/data/products.json",
-               { crossDomain: true }
-          );
-           * 
-           */
-
-          const response = await axios.get("products.json", {
-               // fetch products.json from /public
-          });
-
-          const featuredData = response.data.featured;
-
-          const carouselData = response.data.carousel.items;
-
-          dispatch({
-               type: "FETCH_DATA",
-               payload: response.data,
-          });
-
-          console.log(response.data);
-     };
-
-     useEffect(() => {
-          fetchData();
-     }, []);
 
      return (
           <div>
